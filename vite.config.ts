f7aef9e880db7af49e38303237f1af9abc6c81f9
Anton Westman <anton.westman@wbd.com>
2026-04-07 03:26:06 +0200

    Correct base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { resolve } from 'node:path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/jordenanropar/',
+  base: '/',
   build: {
     outDir: 'dist',
     rollupOptions: {
