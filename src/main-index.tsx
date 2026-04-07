9a10ade60c080ff487d076c6a4e2eefb10e04247
Anton Westman <anton.westman@wbd.com>
2026-04-07 02:45:06 +0200

    Fix build

--- src/main-index.tsx
+++ src/main-index.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import IndexPage from './pages/IndexPage'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <IndexPage />
   </StrictMode>
--- src/main-stream.tsx
+++ src/main-stream.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import StreamPage from './pages/StreamPage'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <StreamPage />
   </StrictMode>
